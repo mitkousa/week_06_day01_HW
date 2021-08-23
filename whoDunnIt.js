c67694a164cb65f83af280bef2ab42c0f71652b6
mitkousa <uk.dimitar@gmail.com>
2021-08-23 23:43:36 +0100

    HW done

---
+++ whoDunnIt.js
@@ -0,0 +1,198 @@
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
+  
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
+  
+//   const verdict = declareMurderer();
+//   console.log(verdict);
+
+//   Answer: The murderer is Miss Scarlet. The value of the key murderer is added as formatted string to the sentence, which is part of the function declareMurderer which is assigned to the constant variable verdict, then printed in the console.
+
+
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Answer: we cant change the murderer with changeMurderer as the value of murderer has been set with const variable.
+
+
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// Answer: First verdict: The murderer is Mrs Peacock. Second verdict: The murderer is Professor Plum. For the first one is Mrs Peacock as the let variable was changed.
+
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// Answer: The suspects are Miss Scarlet, Professor Plum, Colonel Mustard. - Mrs Peacock was changed in the function as suspectThree is let. 
+// Suspect three is Mrs Peacock - as it reads let variable cause is out of the function that changes it.
+
+
+
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+//   };
+  
+//   const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+//   }
+  
+//   changeWeapon('Revolver');
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+  
+//   Answer: The weapon is the Revolver. cause we are setting the weapon from scenarion to equal the parameter of the function changeWeapon, where later on we passing it Revolver.
+
+
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Answer: The murderer is Mrs White - as first murderer got changed to Mr WebGL2RenderingContext, but the we change murderer to Mrs White, store it in plotTwist and prints as result murderer = Mrs White, then declareMurderer takes the value of Mrs White and attaches it to the rest of the sentence.
+
+
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Answer: The murderer is Mr Green as after Professor Plum it has been stored into var murderer(=Mr Green) which cant be changed.
+
+
+
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+//   const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+  
+//   Answer: The weapon is candle stick, as we are passing Dining room to plotTwist and colonel Mustard on unxepectedOutcome we assign them to take the value of the parameter of room and murderer, which makes unexpectedOutcome to assign Candle stick as value of the weapon.
+
+
+
+// let murderer = 'Professor Plum';
+
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// // Answer: The murderer is professor Plum. as in the if statement we are trying to reassign the let murderer as string and not the value of the let.
